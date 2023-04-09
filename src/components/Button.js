0e7e349555e199f20e1c14073a136ece3c4169e7
Christinalulu <christinalu.fugel96@gmail.com>
2023-04-09 12:53:57 +0200

    header done

---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+
+const Button = () => {
+   return (
+      <button className="text-3xl text-lime-400 hover:text-green-400">
+        <ion-icon name="cart-outline"></ion-icon>
+      </button>
+   )
+}
+export default Button
